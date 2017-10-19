3dddc35fc11d5631d4665cf5c753f38b0ade8ad0
Shady Atef <shadyoatef@gmail.com>
2017-10-19 13:56:43 +0200

    Removed the Queue Demo

--- Demo/PIC32MX_MPLAB/main.c
+++ Demo/PIC32MX_MPLAB/main.c
@@ -82,10 +82,6 @@
 /* Kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
-#include "queue.h"
-#include "timers.h"
-/* Standard demo includes. */
-#include "partest.h"
 
 /* Hardware specific includes. */
 #include "ConfigPerformance.h"
@@ -96,112 +92,22 @@
 #pragma config CP = OFF, BWP = OFF, PWP = OFF
 
 
-/* Priorities at which the tasks are created. */
-#define mainQUEUE_RECEIVE_TASK_PRIORITY		( tskIDLE_PRIORITY + 2 )
-#define	mainQUEUE_SEND_TASK_PRIORITY		( tskIDLE_PRIORITY + 1 )
-
-/* The rate at which data is sent to the queue.  The 200ms value is converted
-to ticks using the portTICK_PERIOD_MS constant. */
-#define mainQUEUE_SEND_FREQUENCY_MS			( 200 / portTICK_PERIOD_MS )
-
-/* The number of items the queue can hold.  This is 1 as the receive task
-will remove items as they are added, meaning the send task should always find
-the queue empty. */
-#define mainQUEUE_LENGTH					( 1 )
-
-/* Values passed to the two tasks just to check the task parameter
-functionality. */
-#define mainQUEUE_SEND_PARAMETER			( 0x1111UL )
-#define mainQUEUE_RECEIVE_PARAMETER			( 0x22UL )
-
-/* The period of the blinky software timer.  The period is specified in ms and
-converted to ticks using the portTICK_PERIOD_MS constant. */
-#define mainBLINKY_TIMER_PERIOD				( 50 / portTICK_PERIOD_MS )
-
-/* The LED used by the communicating tasks and the blinky timer respectively. */
-#define mainTASKS_LED						( 1 )
-#define mainTIMER_LED						( 0 )
-
-/* Misc. */
-#define mainDONT_BLOCK						( 0 )
-
-/*-----------------------------------------------------------*/
-
-
-/* The queue used by both tasks. */
-static QueueHandle_t xQueue = NULL;
-
-/*
- * The tasks as described in the comments at the top of this file.
- */
-static void prvQueueReceiveTask( void *pvParameters );
-static void prvQueueSendTask( void *pvParameters );
-
-/*
- * The callback function for the blinky software timer, as described at the top
- * of this file.
- */
-static void prvBlinkyTimerCallback( TimerHandle_t xTimer );
 
 
 
 
-/*
- * Set up the hardware ready to run this demo.
- */
-static void prvSetupHardware( void );
-
-/*
 
 
 /*-----------------------------------------------------------*/
 
-/*
- * Create the demo tasks then start the scheduler.
- */
+void prvSetupHardware( void );
+
 int main( void )
 {
 	/* Prepare the hardware to run this demo. */
 	prvSetupHardware();
-
-	/* The mainCREATE_SIMPLE_BLINKY_DEMO_ONLY setting is described at the top
-	of this file. */
-	
-TimerHandle_t xTimer;
-
-	/* Create the queue. */
-	xQueue = xQueueCreate( mainQUEUE_LENGTH, sizeof( unsigned long ) );
-
-	if( xQueue != NULL )
-	{
-		/* Create the two tasks as described in the comments at the top of this
-		file. */
-		xTaskCreate( prvQueueReceiveTask,					/* The function that implements the task. */
-					"Rx", 									/* The text name assigned to the task - for debug only as it is not used by the kernel. */
-					configMINIMAL_STACK_SIZE, 				/* The size of the stack to allocate to the task. */
-					( void * ) mainQUEUE_RECEIVE_PARAMETER, /* The parameter passed to the task - just to check the functionality. */
-					mainQUEUE_RECEIVE_TASK_PRIORITY, 		/* The priority assigned to the task. */
-					NULL );									/* The task handle is not required, so NULL is passed. */
-
-		xTaskCreate( prvQueueSendTask, "TX", configMINIMAL_STACK_SIZE, ( void * ) mainQUEUE_SEND_PARAMETER, mainQUEUE_SEND_TASK_PRIORITY, NULL );
-
-		/* Create the blinky software timer as described at the top of this
-		file. */
-		xTimer = xTimerCreate( 	"Blinky",					/* A text name, purely to help debugging. */
-								( mainBLINKY_TIMER_PERIOD ),/* The timer period. */
-								pdTRUE,						/* This is an auto-reload timer, so xAutoReload is set to pdTRUE. */
-								( void * ) 0,				/* The ID is not used, so can be set to anything. */
-								prvBlinkyTimerCallback		/* The callback function that inspects the status of all the other tasks. */
-							);
-
-		if( xTimer != NULL )
-		{
-			xTimerStart( xTimer, mainDONT_BLOCK );
-		}
-
 		/* Start the tasks and timer running. */
 		vTaskStartScheduler();
-	}
 	
 	
 
@@ -209,7 +115,7 @@ TimerHandle_t xTimer;
 }
 /*-----------------------------------------------------------*/
 
-static void prvSetupHardware( void )
+void prvSetupHardware( void )
 {
 	/* Configure the hardware for maximum performance. */
 	vHardwareConfigurePerformance();
@@ -219,8 +125,6 @@ static void prvSetupHardware( void )
 
 	portDISABLE_INTERRUPTS();
 
-	/* Setup the digital IO for the LED's. */
-	vParTestInitialise();
 }
 /*-----------------------------------------------------------*/
 
@@ -306,66 +210,4 @@ volatile unsigned long ul = 0;
 	__asm volatile( "ei" );
 }
 
-
-
-static void prvQueueSendTask( void *pvParameters )
-{
-TickType_t xNextWakeTime;
-const unsigned long ulValueToSend = 100UL;
-
-	/* Check the task parameter is as expected. */
-	configASSERT( ( ( unsigned long ) pvParameters ) == mainQUEUE_SEND_PARAMETER );
-
-	/* Initialise xNextWakeTime - this only needs to be done once. */
-	xNextWakeTime = xTaskGetTickCount();
-
-	for( ;; )
-	{
-		/* Place this task in the blocked state until it is time to run again.
-		The block time is specified in ticks, the constant used converts ticks
-		to ms.  While in the Blocked state this task will not consume any CPU
-		time. */
-		vTaskDelayUntil( &xNextWakeTime, mainQUEUE_SEND_FREQUENCY_MS );
-
-		/* Send to the queue - causing the queue receive task to unblock and
-		toggle the LED.  0 is used as the block time so the sending operation
-		will not block - it shouldn't need to block as the queue should always
-		be empty at this point in the code. */
-		xQueueSend( xQueue, &ulValueToSend, 0U );
-	}
-}
-/*-----------------------------------------------------------*/
-
-static void prvQueueReceiveTask( void *pvParameters )
-{
-unsigned long ulReceivedValue;
-
-	/* Check the task parameter is as expected. */
-	configASSERT( ( ( unsigned long ) pvParameters ) == mainQUEUE_RECEIVE_PARAMETER );
-
-	for( ;; )
-	{
-		/* Wait until something arrives in the queue - this task will block
-		indefinitely provided INCLUDE_vTaskSuspend is set to 1 in
-		FreeRTOSConfig.h. */
-		xQueueReceive( xQueue, &ulReceivedValue, portMAX_DELAY );
-
-		/*  To get here something must have been received from the queue, but
-		is it the expected value?  If it is, toggle the LED. */
-		if( ulReceivedValue == 100UL )
-		{
-			vParTestToggleLED( mainTASKS_LED );
-			ulReceivedValue = 0U;
-		}
-	}
-}
-/*-----------------------------------------------------------*/
-
-static void prvBlinkyTimerCallback( TimerHandle_t xTimer )
-{
-	/* This function is called when the blinky software time expires.  All the
-	function does is toggle the LED.  LED mainTIMER_LED should therefore toggle
-	with the period set by mainBLINKY_TIMER_PERIOD. */
-	vParTestToggleLED( mainTIMER_LED );
-}
 
